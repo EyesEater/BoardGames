2d65fdd7a7ac53c24fff5ea3393d4f3662cd75e3
EyesEater <xavier.fernandez@toulouse.miage.fr>
2021-04-07 14:33:13 +0200

    Updating column management for Las Vegas

--- public/javascripts/appAngular.js
+++ public/javascripts/appAngular.js
@@ -1,18 +1,79 @@
 const app = angular.module('boardgame', ['LocalStorageModule', 'ngDragDrop'], $locationProvider => {
     $locationProvider.html5Mode({
         enabled: true,
-        requireBase: false
+        requireBase: false,
+        rewriteLinks: false
     });
 });
 
-app.controller('playController', ($scope, $http, $location, localStorageService) => {
-    localStorageService.remove('LasVegasConfig');
-    if (!localStorageService.get('LasVegasConfig'))
-        $http.post(`/play/${$location.path().split('/')[2]}`).then((data, error) => {
-            if (error) throw error
-            $scope.config = data.data;
-            localStorageService.set('LasVegasConfig', data.data);
+app.directive('backImg', () => {
+    return function(scope, element, attrs){
+        var url = attrs.backImg;
+        element.css({
+            'background-image': 'url(' + url +')',
+            'background-size' : 'cover'
         });
-    else
-        $scope.config = localStorageService.get('LasVegasConfig');
+    };
+});
+
+app.controller('playController', ($scope, $http, $location, localStorageService, $q) => {
+
+    $http.post(`/play/${$location.path().split('/')[2]}`).then((data, error) => {
+        if (error) throw error
+        $scope.config = data.data;
+        $scope.userDices = {};
+        $scope.userDices.dices = data.data.users[`${data.data.user.id}`].des;
+        localStorageService.set('LasVegasConfig', data.data);
+    });
+
+    $scope.goldenNugget = [];
+    $scope.caesarPalace = [];
+    $scope.theMirage = [];
+    $scope.sahara = [];
+    $scope.luxor = [];
+    $scope.circusCircus = [];
+
+    $scope.beforeDrop = (event, ui, column) => {
+        let deferred = $q.defer();
+        if ($scope.d.dice !== column && $scope.d.dice <= 6) {
+            deferred.reject()
+        } else {
+            deferred.resolve()
+        }
+        return deferred.promise;
+    }
+
+    $scope.onStart = (event, ui, d) => {
+        $scope.d = d;
+    }
+
+    $scope.putAll = (column) => {
+        angular.forEach($scope.userDices.dices, (key, dice) => {
+            if (key.dice === column) {
+                switch (column) {
+                    case 1:
+                        $scope.goldenNugget.push(key);
+                        break;
+                    case 2:
+                        $scope.caesarPalace.push(key);
+                        break;
+                    case 3:
+                        $scope.theMirage.push(key);
+                        break;
+                    case 4:
+                        $scope.sahara.push(key);
+                        break;
+                    case 5:
+                        $scope.luxor.push(key);
+                        break;
+                    case 6:
+                        $scope.circusCircus.push(key);
+                        break;
+                    default:
+                        break;
+                }
+                delete $scope.config.users[`${key.user.id}`].des[`${dice}`];
+            }
+        });
+    }
 });
--- routes/play.js
+++ routes/play.js
@@ -7,7 +7,7 @@ let User = require('../models/User');
 let Participate = require('../models/Participate');
 const sessionChecker = require('../services/sessionChecker');
 
-async function getLasVegasConfig(participate) {
+async function getLasVegasConfig(participate, user) {
     const game = await Game.findByPk(participate.gameId);
     const users = await User.findAll({ include: { model: Lobby, where: { gameId: game.id }}});
 
@@ -19,21 +19,30 @@ async function getLasVegasConfig(participate) {
         config = fs.readFileSync(path, 'utf8');
     } else {
         config.users = {};
+        config.user = user;
         let colors = ['White', 'Black', 'Blue', 'Green', 'Magenta', 'Red', 'Yellow'];
 
         users.forEach(user => {
             if (!config.users[`${user.id}`]) {
                 config.users[`${user.id}`] = {};
                 config.users[`${user.id}`].user = user;
-                let i = Math.floor(Math.random() * colors.length);
-                config.users[`${user.id}`].color = colors[i];
-                colors = colors.slice(0,i).concat(colors.slice(i+1, colors.length));
-                config.users[`${user.id}`].des = [];
+                config.users[`${user.id}`].des = {};
             }
-            config.users[`${user.id}`].des.push(Math.floor((Math.random() * 6) + 1) * 2);
-            for(let i=0; i<7; i++) {
-                config.users[`${user.id}`].des.push(Math.floor((Math.random() * 6) + 1));
+            config.users[`${user.id}`].des['0'] = {};
+            config.users[`${user.id}`].des['0'].dice = (Math.floor((Math.random() * 6) + 1) * 2);
+            config.users[`${user.id}`].des['0'].user = user;
+
+            let colorIndex = Math.floor(Math.random() * colors.length);
+
+            for(let i=0; i<8; i++) {
+                config.users[`${user.id}`].des[`${i}`] = {};
+                config.users[`${user.id}`].des[`${i}`].dice = (Math.floor((Math.random() * 6) + 1));
+                config.users[`${user.id}`].des[`${i}`].user = user;
+                config.users[`${user.id}`].des[`${i}`].color = colors[colorIndex];
+                config.users[`${user.id}`].des[`${i}`].doubleDice = false;
             }
+            config.users[`${user.id}`].des['0'].doubleDice = true;
+            colors = colors.slice(0,colorIndex).concat(colors.slice(colorIndex+1, colors.length));
         });
 
         let cartes = [100000, 90000, 80000, 60000, 40000, 20000];
@@ -70,7 +79,7 @@ router.route('/:participateId').get(sessionChecker, function(req, res) {
     });
 }).post(sessionChecker, ((req, res) => {
     Participate.findByPk(req.params.participateId).then((participate) => {
-        getLasVegasConfig(participate).then(config => {
+        getLasVegasConfig(participate, req.session.user).then(config => {
             res.send(config);
         });
     });
